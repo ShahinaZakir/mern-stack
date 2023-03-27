9d8b07c7d1166ea4f12aeeafa98b68ae858d3c07
Shahina Zakir <shahizakir06@gmail.com>
2023-03-27 14:42:58 +0500

    maping the array

---
+++ js/findexArray.js
@@ -0,0 +1,3 @@
+const number=[2,4,3,5];
+array.number.findindex((n)=> n % 2 !==0);
+console.log(number);
---
+++ js/mapArray.js
@@ -0,0 +1,3 @@
+const array=[3,4,8,6];
+arr=array.map((n)=> n *2); //6,8,16,12
+console.log(arr);
